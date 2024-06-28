4b45d25e8c92cbb28e629089d463c409fa6d6d82
Andrei Shabaev <76488291+SADfranco@users.noreply.github.com>
2024-06-28 20:47:20 +0300

    Update api_requests.py

--- tsum_tests/helper/api_requests.py
+++ tsum_tests/helper/api_requests.py
@@ -2,24 +2,15 @@ import requests
 import allure
 from tsum_tests.utils import attach
 
+class ApiCall:
+    def api_get(method, endpoint, **kwargs):
+        with allure.step(f"Send API {method} Request to endpoint {endpoint}"):
+            response = requests(method=method.upper(), url="https://api.tsum.ru" + endpoint, **kwargs)
 
-def api_get(endpoint, **kwargs):
-    with allure.step(f"API GET Request to endpoint {endpoint}"):
-        response = requests.get(url="https://api.tsum.ru" + endpoint, **kwargs)
+            attach.request_url_and_body(response)
+            attach.response_json_and_cookies(response)
+            attach.logging_response(response)
 
-        attach.request_url_and_body(response)
-        attach.response_json_and_cookies(response)
-        attach.logging_response(response)
-
-        return response
-
-
-def api_post(endpoint, **kwargs):
-    with allure.step(f"API POST Request to endpoint {endpoint}"):
-        response = requests.post(url="https://api.tsum.ru" + endpoint, **kwargs)
-
-        attach.request_url_and_body(response)
-        attach.response_json_and_cookies(response)
-        attach.logging_response(response)
-
-        return response
+            return response
+            
+api_call = ApiCall()
